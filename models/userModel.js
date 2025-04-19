2ea147dae911d54efd10f0acfe3aa0f339216587
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-04-19 20:11:19 -0300

    Mensagem do commit

---
+++ models/userModel.js
@@ -0,0 +1,12 @@
+const db = require('../config/db');
+
+const User = {
+  findAll: (callback) => {
+    db.query('SELECT * FROM users', (err, results) => {
+      if (err) return callback(err);
+      return callback(null, results);
+    });
+  }
+};
+
+module.exports = User;
