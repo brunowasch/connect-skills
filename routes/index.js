15df1c7583c49376ca29568bb4bd80020fb98025
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-04-23 20:03:00 -0300

    Adicionei mais uma página para contruir o cadastro nela.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,15 @@
+// routes/index.js
 const express = require('express');
 const router = express.Router();
-const homeController = require('../controllers/homeController');
 
 // Página inicial
-router.get('/', homeController.index);
+router.get('/', (req, res) => {
+  res.render('home', { title: 'Connect Skills - Início' });
+});
+
+// Página de cadastro
+router.get('/cadastro', (req, res) => {
+  res.render('cadastro', { title: 'Cadastro - Connect Skills' });
+});
 
 module.exports = router;
