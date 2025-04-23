279235dbcb59b637266ae7cc7d80ee53aa863f37
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-04-23 19:33:30 -0300

    Ajustei problemas com o express e mudei a LP

---
+++ config/db.js
@@ -0,0 +1,15 @@
+// const mysql = require('mysql2');
+
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'connectskills'
+// });
+
+// connection.connect((err) => {
+//   if (err) throw err;
+//   console.log('Conectado ao banco de dados MySQL!');
+// });
+
+// module.exports = connection;
