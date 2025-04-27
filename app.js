2875768f2a76d63ddfdb8fc43a66b9f7df464eaa
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-04-26 23:55:34 -0300

    Adicionei a pasta src

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express();
 const port = 3000;
 
 // Conexão com banco de dados
-const db = require('./config/db');
+const db = require('./src/config/db');
 
 // Configuração do EJS
+app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Rotas
-const routes = require('./routes/index');
+const routes = require('./src/routes/index');
 app.use('/', routes);
 
 // Iniciando servidor
