b172e84e285ed64d9ba09d8abc1deb3b3f525412
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-07-25 13:05:59 -0300

    Sessão persistente para que o acesso à página funcione corretamente.

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const session = require('express-session');
 const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./src/config/db');
+const MySQLStore = require('express-mysql-session')(session);
 
 const authRoutes = require('./src/routes/authRoutes');
 const usuarioRoutes = require('./src/routes/usuarioRoutes');
@@ -15,19 +16,29 @@ const mainRoutes = require('./src/routes/index');
 const app = express();
 const port = 3000;
 
-// Middleware para leitura de dados dos formulários
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json({ limit: '10mb' }));
 
-// Sessão
+const options = {
+  host: process.env.DB_HOST || 'localhost',
+  port: 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+};
+
+const sessionStore = new MySQLStore(options);
+
+
 app.use(session({
   secret: process.env.SECRET_SESSION || 'default_secret',
   resave: false,
   saveUninitialized: false,
+  store: sessionStore, 
   cookie: {
     httpOnly: true,
-    secure: false, // use true em produção com HTTPS
-    maxAge: 1000 * 60 * 60 * 24 * 7 // 7 dias
+    secure: false, 
+    maxAge: 1000 * 60 * 60 * 24 * 7
   }
 }));
 
@@ -46,9 +57,7 @@ const autenticar = (req, res, next) => {
   next();
 };
 
-// ROTAS
-
-// Rotas públicas
+app.use('/', mainRoutes);
 app.use('/', authRoutes); // /cadastro, /login
 app.use('/usuarios', usuarioRoutes); // cadastro, login, verificação
 
@@ -57,7 +66,7 @@ app.use('/candidatos', candidatoRoutes); // etapa de cadastro e acesso
 app.use('/empresas', empresaRoutes);     // etapa de cadastro e acesso
 
 // Rota principal
-app.use('/', mainRoutes);
+
 
 // Rota de logout
 app.get('/logout', (req, res) => {
