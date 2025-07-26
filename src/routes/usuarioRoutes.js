f73bfaaed9e31cf490c19a38aa6994e517d6c74e
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-07-25 23:46:19 -0300

    Podendo verificar email em outro dispositivo

--- src/routes/usuarioRoutes.js
+++ src/routes/usuarioRoutes.js
@@ -6,6 +6,7 @@ router.post('/cadastrar', usuarioController.criarUsuario);
 router.post('/login', usuarioController.login);
 router.get('/verificar-email', usuarioController.verificarEmail);
 router.get('/aguardando-verificacao', usuarioController.telaAguardandoVerificacao);
+router.get('/status-verificacao', usuarioController.statusVerificacao);
 router.get('/email-verificado', usuarioController.telaEmailVerificado);
 router.post('/reenviar-email', usuarioController.reenviarEmail);
 module.exports = router;
