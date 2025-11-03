54650b9d9af273c16397e66543ce304e1feb0410
Marcelo Rangel Barros <marcelorangelb2006@gmail.com>
2025-11-03 09:55:55 -0300

    fix: Ajustando o app.js para definir encodeId globalmente

--- src/app.js
+++ src/app.js
@@ -120,9 +120,6 @@ app.use((req, res, next) => {
   res.locals.empresa   = req.session.empresa || null;
   res.locals.usuario   = req.session.usuario || res.locals.candidato || res.locals.empresa || null;
   res.locals.sessionRemember = Boolean(req.session?.remember);
-  
-  res.locals.encodeId = encodeId;
-  res.locals.decodeId = decodeId;
   next();
 });
 
